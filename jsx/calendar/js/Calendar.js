589300448a3dca655f6605c16be036ad07366305
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-05-26 01:49:58 +0300

    Update Calendar.js

--- jsx/calendar/js/Calendar.js
+++ jsx/calendar/js/Calendar.js
@@ -42,32 +42,80 @@ const Calendar = function(props) {
             <th scope="col" title="Воскресенье">Вс</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            {makeWeek(props.date, 1)}
-          </tr>
-        </tbody>
+          {makeMonth(props.date)}
       </table>
     </div>
   )
 }
 
-function makeWeek(date, weekNumber) {
+function makeMonth(date) {
+  let weeks = [0,1,2,3,4,5];
   let firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
   if (firstDay === 0) {firstDay = 7};
+  let monthDays = 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
+  let lastDay = new Date(date.getFullYear(), date.getMonth(), monthDays).getDay();
+  if (monthDays === 28) {
+    weeks[5] = -1;
+    if (firstDay === 1) {
+      weeks[4] = -1;
+    }
+  } else if (((firstDay === 6) || (monthDays === 31)) && ((firstDay === 7) || (monthDays > 31)) ){
+    weeks[5] = -1;
+  }
+  let app = (
+    <tbody>
+      {makeWeek(date, weeks[0])}
+      {makeWeek(date, weeks[1])}
+      {makeWeek(date, weeks[2])}
+      {makeWeek(date, weeks[3])}
+      {makeWeek(date, weeks[4])}
+      {makeWeek(date, weeks[5])}
+    </tbody>
+  );
+  return app;
+}
+
+function makeWeek(date, weekNumber) {
+  if (weekNumber === -1) {
+    return null;
+  }
+  let firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+  if (firstDay === 0) {firstDay = 7};
+  let monthDays = 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
   let previousMonthDays = 32 - new Date(date.getFullYear(), date.getMonth() - 1, 32).getDate();
   let week = [];
-  for (let i = 0; i < firstDay - 1; i++) {
-    week[i] = previousMonthDays + i - (firstDay - 2);
-  }
-  for (let i = firstDay - 1; i < 7; i++) {
-    week[i] = i - (firstDay - 2);
+  let isAnotherMonth = [false, false, false, false, false, false, false];
+  for (let i = 0; i < 7; i++) {
+    week[i] = i - firstDay + 2 + weekNumber * 7;
+    if (week[i] < 1) {
+      week[i] += previousMonthDays;
+      isAnotherMonth[i] = true;
+      continue;
+    }
+    if (week[i] > monthDays) {
+      week[i] -= monthDays;
+      isAnotherMonth[i] = true;
+    }
   }
   return (
-    week.map(makeDay)
+    <tr>
+      {makeDay(week[0], isAnotherMonth[0], date)}
+      {makeDay(week[1], isAnotherMonth[1], date)}
+      {makeDay(week[2], isAnotherMonth[2], date)}
+      {makeDay(week[3], isAnotherMonth[3], date)}
+      {makeDay(week[4], isAnotherMonth[4], date)}
+      {makeDay(week[5], isAnotherMonth[5], date)}
+      {makeDay(week[6], isAnotherMonth[6], date)}
+    </tr>
   )
 }
 
-function makeDay(day) {
+function makeDay(day, isAnotherMonth, date) {
+  if (isAnotherMonth) {
+    return <td className="ui-datepicker-other-month">{day}</td>
+  }
+  if (day === date.getDate()) {
+    return <td className="ui-datepicker-today">{day}</td>
+  }
   return <td>{day}</td>
 }
