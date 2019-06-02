7bbf3f0428db46ac7375e10a6ebef1565babb387
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-02 18:37:59 +0300

    Update App.js

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,30 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected : "All"
+    }
+  }
+  filterSelect(filter) {
+    console.log(filter);
+    this.state.selected = filter;
+  }
+  projectFilter(projects) {
+    return (this.state.selected === 'All')
+      ? projects
+      : projects.filter(project => project.category === this.state.selected);
+  }
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.selected}
+          onSelectFilter={(filter) => this.filterSelect(filter)} />
+        <Portfolio projects={this.projectFilter(this.props.projects)} />
+      </div>
+    );
+  }
+}
