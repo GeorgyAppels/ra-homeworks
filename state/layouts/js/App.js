c7837d718b693ea80002458f1f6769293db079aa
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-02 20:33:07 +0300

    Update App.js

--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,15 +4,26 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      markup : VIEW_MODULE
+    };
+  }
   render() {
     return (
       <div>
         <div className="toolbar">
           <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
+            icon={this.state.markup}
+            onSwitch={() => {
+              console.log("сменился тип вывода");
+              this.setState({
+                markup: (this.state.markup === VIEW_LIST) ? VIEW_MODULE : VIEW_LIST
+              });
+            } } />
         </div>
-        {this.renderLayout(true)}
+        {this.renderLayout(this.state.markup === VIEW_MODULE)}
       </div>
     );
   }
