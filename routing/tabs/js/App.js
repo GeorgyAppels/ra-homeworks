032596fd3738eb90a783cec8ba259f70263426a0
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-15 11:41:10 +0300

    Update App.js

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,18 +1,20 @@
-const App = function() {
-  return (
-    <div className="tabs">
-      <nav className="tabs__items">
-        <a className="tabs__item" href="#/">Рефераты</a>
-        <a className="tabs__item" href="#/creator">Криэйтор</a>
-        <a className="tabs__item" href="#/fortune">Гадалка</a>
-      </nav>
-      <Router>
-        <div className="tabs__content">
-          <Route path="#/" exact component={Essay} />
-          <Route path="#/creator" component={Creator} />
-          <Route path="#/fortune" component={Fortune} />
-        </div>
-      </Router>
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className="tabs">
+        <nav className="tabs__items">
+          <a className="tabs__item" href="#/">Рефераты</a>
+          <a className="tabs__item" href="#/creator">Криэйтор</a>
+          <a className="tabs__item" href="#/fortune">Гадалка</a>
+        </nav>
+        <Router>
+          <div className="tabs__content">
+            <Route path="/" exact component={Essay} />
+            <Route path="/creator" component={Creator} />
+            <Route path="/fortune" component={Fortune} />
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
