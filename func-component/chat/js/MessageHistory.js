838e0f8c02d4477fc65730f6c8651c2b34f2c189
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-16 14:20:23 +0300

    Update MessageHistory.js

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function MessageHistory(props) {
+  return (
+    <ul>
+      {props.list.map((message) => (
+        (message.type === "message") ? Message({from: message.from, message: message}) :
+        (message.type === "response") ? Response({from: message.from, message: message}) :
+        Typing({from: message.from, message: message})
+      ))}
+    </ul>
+  )
+}
