20619b10cee68b9b4c9a95cdc2fcbab88cfb80de
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-16 14:19:43 +0300

    Update Stars.js

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,19 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars(props) {
+
+  if ((props.count >= 1) && (props.count <= 5)) {
+    let stars = [];
+    for (let i = 1; i <= props.count; i++) {
+      stars.push(Star())
+    }
+    console.log(stars[0]);
+    return (
+      <ul className="card-body-stars u-clearfix">
+        <li>
+          {stars.map((el) => el)}
+        </li>
+      </ul>
+    )
+  }
 }
