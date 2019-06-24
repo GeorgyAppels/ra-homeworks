02a56ab517d7037830c47cd2f33c4e425e7f525c
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-25 00:52:42 +0300

    Update Cart.js

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,5 +1,10 @@
 class Cart extends React.Component {
-
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return ((nextProps.items.length !== this.props.items.length) && (this.props.isOpen)) || (nextProps.isOpen !== this.props.isOpen)
+  }
   render() {
     return (
       <CartView {...this.props} />
