7e11a57521cd2cb3881c1f24e1a1835857c46a00
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-25 00:52:20 +0300

    Update ProgressBar.js

--- lifecycle/lib/js/ProgressBar.js
+++ lifecycle/lib/js/ProgressBar.js
@@ -1,4 +1,18 @@
 class ProgressBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: Math.round(this.props.completed / this.props.total * 100)
+    }
+  }
+  componentDidMount() {
+    console.log(this.state.progress)
+  }
+  componentWillReceiveProps() {
+    this.setState({
+      progress: Math.round(this.props.completed / this.props.total * 100)
+    });
+  }
   render() {
     return (
       <canvas id="progressCanvas" className="progress" />
