b9f81555942d6e3ca8b8ddfd1f6e8240b358195a
GeorgyAppels <48926950+GeorgyAppels@users.noreply.github.com>
2019-06-27 09:16:46 +0300

    Update Header.js

--- routing-xhr/latin/js/Header.js
+++ routing-xhr/latin/js/Header.js
@@ -1,8 +1,10 @@
+const {withRouter} = ReactRouterDOM
+
 const HeaderComponent = ({ match }) => (
   <nav className="navbar navbar-light bg-light">
     {match && match.params.id
       ? <p className="navbar-brand">Уникальный идентификатор статьи: { match.params.id}</p>
-      : <p className="navbar-brand">Статья не выбрана</p> 
+      : <p className="navbar-brand">Статья не выбрана</p>
     }
   </nav>
 );
